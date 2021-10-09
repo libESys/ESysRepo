6d513b901afc4789748ebb05ad85257d742eeb10
Michel Gillet <michel.gillet@libesys.org>
2021-10-09 21:46:52 +0300

    Generate HTML report from valgrind output
    Ref #42

--- scripts/valgrind_unittests.sh
+++ scripts/valgrind_unittests.sh
@@ -6,17 +6,28 @@ echo "pwd = "`pwd`
 source scripts/start_ssh_agent
 
 cd build_dev
+mkdir -p build/cmake
+cd build
+cd cmake
 
-valgrind --leak-check=full \
-         --show-leak-kinds=all \
-         --track-origins=yes \
-         --verbose \
-         --log-file=valgrind-out.txt \
-         ./build/cmake/bin/esysrepo_t --logger=HRF,test_suite,stdout:JUNIT,all,report_junit.txt
+make valgrind-html-esysrepo_t -j`nproc --all`
 
-mkdir -p ../public/logs
-cp *_junit.txt ../public/logs
-cp valgrind-out.txt ../public/logs
+# valgrind --leak-check=full \
+#          --show-leak-kinds=all \
+#          --track-origins=yes \
+#          --verbose \
+#          --log-file=valgrind-out.txt \
+#          ./build/cmake/bin/esysrepo_t --logger=HRF,test_suite,stdout:JUNIT,all,report_junit.txt \
+#          --xml-file=valgrind-out.xml
+
+# valgrind-ci valgrind-out.xml --number-of-errors
+
+mkdir -p ../../../public/logs
+mkdir -p ../../../public/valgrind
+cp src/esysrepo/src/esys/repo/*_junit.txt ../../../public/logs
+cp src/esysrepo/src/esys/repo/esysrepo_t-valgrind-out.* ../../../public/logs
+cp src/esysrepo/src/esys/repo/esysrepo_t-valgrind-out.* ../../../public/valgrind
+cp -R src/esysrepo/src/esys/repo/test/valgrind_html/* ../../../public/valgrind
 
 echo "pwd = "`pwd`
 echo "${TXT_S}Valgrind unit test done.${TXT_CLEAR}"
