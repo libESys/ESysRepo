4e1fdc6a85c6d93471a4ba3f27b88b46a9e944e0
Michel Gillet <michel.gillet@libesys.org>
2021-10-09 09:23:00 +0300

    Create clang-tidy report and deploy it
    Ref #40

---
+++ scripts/run_clang_tidy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "${TXT_S}Run cland-tidy ...${TXT_CLEAR}"
+echo "pwd = "`pwd`
+
+source scripts/start_ssh_agent
+
+cd build_dev
+cd build
+cd cmake
+make tidy-esysrepo-html -j`nproc --all`
+RESULT_TIDY=$?
+
+mkdir -p ../../../public/clang_tidy
+cp -R src/esysrepo/src/esys/repo/esysrepo-clang_tidy.html ../../../public/clang_tidy
+cp -R src/esysrepo/src/esys/repo/esysrepo-clang_tidy.log ../../../public/clang_tidy
+
+if [ ! ${RESULT_TIDY} -eq 0 ]; then
+   echo "${TXT_W}Clang-tidy failed.${TXT_CLEAR}"
+fi
+
+echo "pwd = "`pwd`
+echo "${TXT_S}Run cland-tidy done.${TXT_CLEAR}"
