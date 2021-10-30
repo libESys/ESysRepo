6c6880773a2e622a561d72858a52b2828d20d170
Michel Gillet <michel.gillet@libesys.org>
2021-10-30 08:58:34 +0300

    Uploading to codecov
    Ref #51

--- scripts/run_code_coverage.sh
+++ scripts/run_code_coverage.sh
@@ -8,13 +8,17 @@ source scripts/start_ssh_agent
 cd build_dev
 cd build
 cd cmake
-make esysrepo_coverage -j`nproc --all`
+make esysrepo_coverage-cleanup -j`nproc --all`
 RESULT_UT=$?
+
+lcov --list src/esysrepo/src/esys/repo/esysrepo_coverage.info
 
 mkdir -p ../../../public/coverage
 mkdir -p ../../../public/logs
 cp -R src/esysrepo/src/esys/repo/esysrepo_coverage/* ../../../public/coverage
+cp src/esysrepo/src/esys/repo/esysrepo_coverage.info ../../../public/logs/esysrepo_coverage.info
 cp src/esysrepo/src/esys/repo/report.junit ../../../public/logs/esysrepo_junit.txt
+bash <(curl -s https://codecov.io/bash) -f src/esysrepo/src/esys/repo/esysrepo_coverage.info || echo "${TXT_W}Codecov did not collect coverage reports.${TXT_CLEAR}"
 
 if [ ! ${RESULT_UT} -eq 0 ]; then
    echo "${TXT_E}Code coverage on unit tests failed.${TXT_CLEAR}"
